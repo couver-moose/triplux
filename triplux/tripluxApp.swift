67369cc44f60e0ed62c5fd077dce6844711fcc62
dario <dario.dassler@fh-erfurt.de>
2025-07-01 01:38:11 +0200

    Initial Commit

---
+++ triplux/tripluxApp.swift
@@ -0,0 +1,17 @@
+//
+//  tripluxApp.swift
+//  triplux
+//
+//  Created by Dario Daßler on 01.07.25.
+//
+
+import SwiftUI
+
+@main
+struct tripluxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
